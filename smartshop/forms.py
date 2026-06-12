530973c9f2fe6d75ef571c5bddd922a8cee2815f
Hanako Yamada <h-yamada@example.com>
2026-06-12 16:31:08 +0900

    コミットします

--- smartshop/forms.py
+++ smartshop/forms.py
@@ -2,3 +2,60 @@ from django import forms
 from smartshop.models import User
 from django.core.exceptions import ValidationError
 
+class UserCreateForm(forms.Form):
+    user_id  = forms.CharField(label="会員ID", max_length=128, widget=forms.TextInput(attrs={"class": "form-control"}))
+    password = forms.CharField(label="パスワード", max_length=256,widget=forms.PasswordInput(render_value=True, attrs={"class": "form-control"}))
+    confirm_password = forms.CharField(label="パスワード（確認）",widget=forms.PasswordInput(render_value=True, attrs={"class": "form-control"}))
+    name = forms.CharField(label="名前", max_length=128, widget=forms.TextInput(attrs={"class": "form-control"}))
+    address = forms.CharField(label="住所", max_length=256, widget=forms.TextInput(attrs={"class": "form-control"}))
+
+    def clean_user_id(self):
+        value = self.cleaned_data["user_id"]
+        if User.objects.filter(user_id=value).exists():
+            raise ValidationError("このIDは登録されています")
+        return value
+
+    def clean_password(self):
+        value = self.cleaned_data["password"]
+        return value
+
+    def clean_name(self):
+        value = self.cleaned_data["name"]
+        return value
+
+    def clean_address(self):
+        value = self.cleaned_data["address"]
+        return value
+    
+    
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password:
+            if password != confirm_password:
+                raise forms.ValidationError("パスワードが一致しません")
+
+        return cleaned_data
+
+
+class UserLoginForm(forms.Form):
+    user_id = forms.CharField(label="e-mail", max_length=128, widget=forms.TextInput(attrs={"class": "form-control"}))
+    password = forms.CharField(label="パスワード", max_length=256, widget=forms.PasswordInput(render_value=True, attrs={"class": "form-control"}))
+
+    
+    def clean(self):
+        user_id = self.cleaned_data["user_id"]
+        password = self.cleaned_data["password"]
+        try:
+            user = User.objects.get(user_id=user_id)
+        except User.DoesNotExist:
+            raise ValidationError("登録されていないIDです")
+        if user.password != password:
+            raise ValidationError("パスワードが違います")
+        
+
+    
+
--- smartshop/views.py
+++ smartshop/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.views.generic import View
-from smartshop.models import Item,Category
-# from smartshop.forms import UserCreateForm
+from smartshop.models import Item,Category,User
+from smartshop.forms import UserCreateForm
 # from smartshop.forms import UserLoginForm
 
 def index(request):
@@ -90,5 +90,84 @@ class itemDetail(View):
 
 class cart(View):
     def post(self, request, *args, **kwargs):
-        
-        return
+        buy_amount=int(request.POST.get('amount'))
+        item_id=int(request.POST.get("items_id"))
+        queryset = Item.objects.get(item_id=item_id)
+        context = {
+            "items": queryset,
+            "amount":buy_amount,
+            }
+        return render(request, "smartshop/cart.html", context)
+    
+class UserCreate(View):
+
+    def get(self, request, *args, **kwargs):
+        form = UserCreateForm()
+        context = {
+            "form": form,
+        }
+        return render(request, "smartshop/registUser.html", context)
+
+    
+
+    def post(self, request, *args, **kwargs):
+        #入力したものの検証
+        #OKならUserConfirmに値を渡す
+        pass
+        # new_user.save()
+        # # department_listビューの呼び出し
+        # return render(request, "smartshop/registUser.html", context)
+  
+class UserConfirm(View):
+
+    def get(self, request, *args, **kwargs):
+        pass
+
+    
+
+    def post(self, request, *args, **kwargs):
+        form = UserCreateForm(request.POST)
+        queryset = User.objects.all().order_by("-user_id")
+        context={
+           "user_list":queryset
+        }
+
+        if not form.is_valid():  # バリデーション実施、問題があった場合の処理
+            context = {
+                "form": form,
+                "user_list": queryset,
+            }
+            return render(request, "smartshop/registUser.html", context)
+
+        new_user = User()
+        # バリデーションに問題がなかったデータの取り出しDepartmentモデルにセット
+        new_user.password = form.cleaned_data.get("password")
+        new_user.name = form.cleaned_data.get("name")
+        new_user.user_id = form.cleaned_data.get("user_id")
+        new_user.address = form.cleaned_data.get("address")
+        # new_user.save()
+        # department_listビューの呼び出し
+        context={
+            "new_user":new_user
+        }
+        return render(request, "smartshop/registUserConfirm.html", context)
+    
+class UserCommit(View):
+
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        new_user = User()
+        # バリデーションに問題がなかったデータの取り出しDepartmentモデルにセット
+        new_user.password = request.POST.get("password")
+        new_user.name = request.POST.get("name")
+        new_user.user_id = request.POST.get("user_id")
+        new_user.address = request.POST.get("address")
+        new_user.save()
+
+        context={
+            "user_name":new_user.name
+        }
+
+        return render(request, "smartshop/registUserCommit.html",context)
